f5c8b57403ccc2702a84187a6ad2c18bcf3c2675
천일영 <dlfdud0304@naver.com>
2026-04-08 23:05:10 +0900

    feat(HomePage.tsx): 홈화면 작성

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export function HomePage() {
+
+  return (
+    <div className="flex w-full h-full font-['Nanum_Myeongjo'] items-center justify-center">
+        <h1 className="text-4xl font-bold">홈</h1>
+    </div>
+  );
+}
