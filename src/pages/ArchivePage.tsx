45b3040d60216ed0dd07d8b29369c12dc04e2f24
천일영 <dlfdud0304@naver.com>
2026-04-08 23:06:54 +0900

    feat(ArchivePage.tsx): 아카이브화면 작성

---
+++ src/pages/ArchivePage.tsx
@@ -0,0 +1,8 @@
+export function ArchivePage() {
+
+  return (
+    <div className="flex w-full h-full font-['Nanum_Myeongjo'] items-center justify-center">
+        <h1 className="text-4xl font-bold">보관함</h1>
+    </div>
+  );
+}
