5214f4ba6eeadc71f001c3f941a0d71e9ae40acf
천일영 <dlfdud0304@naver.com>
2026-04-08 23:06:33 +0900

    feat(RecommendPage.tsx): 추천화면 작성

---
+++ src/pages/RecommendPage.tsx
@@ -0,0 +1,8 @@
+export function RecommendPage() {
+
+  return (
+    <div className="flex w-full h-full font-['Nanum_Myeongjo'] items-center justify-center">
+        <h1 className="text-4xl font-bold">추천</h1>
+    </div>
+  );
+}
