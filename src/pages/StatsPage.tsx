db0ff0dcc7d8518e43cd5fa14ebdd6828369ef96
천일영 <dlfdud0304@naver.com>
2026-04-08 23:05:58 +0900

    feat(StatsPage.tsx): 통계화면 작성

---
+++ src/pages/StatsPage.tsx
@@ -0,0 +1,8 @@
+export function StatsPage() {
+
+  return (
+    <div className="flex w-full h-full font-['Nanum_Myeongjo'] items-center justify-center">
+        <h1 className="text-4xl font-bold">통계</h1>
+    </div>
+  );
+}
