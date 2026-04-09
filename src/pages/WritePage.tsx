d42e578f46e38fdfddb016489f65fb9e36e349f2
천일영 <dlfdud0304@naver.com>
2026-04-09 20:31:28 +0900

    feat(WritePage.tsx): 일기 작성 화면 임시 구현

---
+++ src/pages/WritePage.tsx
@@ -0,0 +1,7 @@
+export function WritePage() {
+  return (
+    <div className="flex w-full h-full font-['Nanum_Myeongjo'] items-center justify-center">
+      <h1 className="text-4xl font-bold">글쓰기</h1>
+    </div>
+  );
+}
