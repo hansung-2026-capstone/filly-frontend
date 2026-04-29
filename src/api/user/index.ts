bf9099d21c5030a83c2057c3bc6f74f4bcc05900
juyeon707 <juyeon7007@gmail.com>
2026-04-29 20:58:18 +0900

    feat(user/index.ts): 유저 데이터 인터페이스 추가

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,11 +1,20 @@
 import { api } from "../instance";
 
-export interface UserMe {
+export interface UserData {
+  id: number;
   nickname: string;
-  profileImageUrl?: string;
+  currentAvatarUrl: string | null;
+  currentBgUrl: string | null;
+  backgroundTheme: string | null;
+  createdAt: string;
 }
 
 export const getMe = async (): Promise<UserMe> => {
   const { data } = await api.get<{ data: UserMe }>("/api/v1/users/me");
+export interface UserResponse {
+  success: boolean;
+  data: UserData;
+  message: string | null;
+}
   return data.data;
 };
