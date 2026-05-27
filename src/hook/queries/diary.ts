3e8e1caaa64dc081f2f39aa00b008f24eb7b3548
kmkang <wowkmini15@gmail.com>
2026-05-28 03:47:00 +0900

    feat(diary.ts): 일기 상세 조회 쿼리 훅 추가

--- src/hook/queries/diary.ts
+++ src/hook/queries/diary.ts
@@ -7,6 +7,7 @@ import {
 import {
   createDraft,
   deleteDiary,
+  getDiary,
   getDiaries,
   saveDiary,
   updateDiary,
@@ -33,8 +34,18 @@ export const monthlyDiariesQueryOptions = (year: number, month: number) =>
     queryFn: () => getDiaries(year, month),
   });
 
+export const diaryDetailQueryOptions = (id: number) =>
+  queryOptions({
+    queryKey: queryKeys.diaries.detail(id),
+    queryFn: () => getDiary(id),
+  });
+
 export function useMonthlyDiariesQuery(year: number, month: number) {
   return useQuery(monthlyDiariesQueryOptions(year, month));
+}
+
+export function useDiaryQuery(id: number) {
+  return useQuery(diaryDetailQueryOptions(id));
 }
 
 export function useCreateDraftMutation() {
